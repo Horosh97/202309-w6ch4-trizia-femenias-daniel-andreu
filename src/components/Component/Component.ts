5a0b68e383970285e0e995298ba8465f6a28f5f7
Patricia <patricia.femenias@gmail.com>
2023-10-18 20:00:31 +0200

    Add Component class

---
+++ src/components/Component/Component.ts
@@ -0,0 +1,17 @@
+abstract class Component {
+  private readonly element: Element;
+  private readonly parentElement: Element;
+
+  constructor(tagName: string, parentElement: Element, className: string) {
+    this.element = document.createElement(tagName);
+    this.parentElement = parentElement;
+    this.element.className = className;
+  }
+
+  public render(): void {
+    this.parentElement.appendChild(this.element);
+  }
+  abstract populate(): void;
+}
+
+export default Component;
